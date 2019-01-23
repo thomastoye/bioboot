fd9700b14251a757b2ee7cbcfbd32c03c437572a
Thomas Toye <thomas@toye.io>
2019-01-23 14:19:21 +0100

    Fix motor sounds

--- arduino/mpuMotorControl/mpuMotorControl.ino
+++ arduino/mpuMotorControl/mpuMotorControl.ino
@@ -5,8 +5,9 @@
 
 //Motordriver declarations
 String motorSetting;
+// BTS7960(int RPWM,int LPWM,int L_EN,int R_EN,int _PRESCALER);
 BTS7960 mL(2, 3, 4, 5, 1);
-BTS7960 mR(6, 7, 8, 9, 1);
+BTS7960 mR(9, 8, 7, 6, 1);
 
 #define   LEFT_MOTOR    1
 #define   RIGHT_MOTOR   2
@@ -95,8 +96,44 @@ void setup()
   motorSetting.reserve(40);
   mL.enable();
   mR.enable();
+
+  setPwmFrequencyMEGA2560(2, 1);
+  setPwmFrequencyMEGA2560(3, 1);
+  setPwmFrequencyMEGA2560(9, 1);
+  setPwmFrequencyMEGA2560(8, 1);
 }
 
+void setPwmFrequencyMEGA2560(int pin, int divisor)
+{     
+  byte mode;
+      switch(divisor) {
+      case 1: mode = 0x01; break; 
+      case 2: mode = 0x02; break; 
+      case 3: mode = 0x03; break; 
+      case 4: mode = 0x04; break; 
+      case 5: mode = 0x05; break; 
+      case 6: mode = 0x06; break; 
+      case 7: mode = 0x07; break; 
+      default: return;
+      }
+      
+        switch(pin) {   
+      case 2:  TCCR3B = TCCR3B  & 0b11111000 | mode; break;
+      case 3:  TCCR3B = TCCR3B  & 0b11111000 | mode; break;
+      case 4:  TCCR0B = TCCR0B  & 0b11111000 | mode; break;
+      case 5:  TCCR3B = TCCR3B  & 0b11111000 | mode; break;
+      case 6:  TCCR4B = TCCR4B  & 0b11111000 | mode; break;
+      case 7:  TCCR4B = TCCR4B  & 0b11111000 | mode; break;
+      case 8:  TCCR4B = TCCR4B  & 0b11111000 | mode; break;
+      case 9:  TCCR2B = TCCR0B  & 0b11111000 | mode; break;
+      case 10: TCCR2B = TCCR2B  & 0b11111000 | mode; break;
+      case 11: TCCR1B = TCCR1B  & 0b11111000 | mode; break;
+      case 12: TCCR1B = TCCR1B  & 0b11111000 | mode; break;
+      case 13: TCCR0B = TCCR0B  & 0b11111000 | mode; break;
+      default: return;
+    }
+
+}
 
 
 
